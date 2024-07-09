775a4f8cfdd9903b581c0cd829a4a4c3e15e8e2d
J. Gerhards <g1.jasger@gmail.com>
2024-07-09 13:47:07 +0200

    add code snippet plugin

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"michaelrommel/nvim-silicon",
+		lazy = true,
+		cmd = "Silicon",
+		init = function ()
+			vim.keymap.set('v', '<leader>sc', ':Silicon<CR>');
+		end,
+		config = function ()
+			require("silicon").setup({
+				font = "Fira Code Nerd Font Mono=34;",
+				theme = "Monokai Extended",
+				background = "#45424b",
+				window_title = function ()
+					return vim.fn.fnamemodify(
+						vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
+					)
+				end
+			})
+		end
+	}
+}
