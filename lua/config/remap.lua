521e310d728c21086f2f0f0886d695018f456fea
J. Gerhards <g1.jasger@gmail.com>
2025-04-24 02:40:42 +0200

    add keymaps to toggle inlay hints and diagnostics display type
    Add a new keymap to toggle inlay hints and another one to toggle
    diagnostics from text to line mode and vise versa.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,6 +3,20 @@ vim.g.mapleader = " "
 -- open search
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex);
 
+-- toggle inlay hints
+vim.keymap.set('n', '<leader>ti', function () vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end);
+
+-- toggle diagnostics from text to line mode
+vim.keymap.set('n', '<leader>td', function ()
+    local config = vim.diagnostic.config(nil)
+    if config ~= nil then
+        vim.diagnostic.config({
+            virtual_text = not config.virtual_text,
+            virtual_lines = not config.virtual_lines
+        })
+    end
+end);
+
 -- Moving stuff that's selected
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
