1170d6a5cfaf5ec670bf66c7c71fb80d2b1cef74
J. Gerhards <g1.jasger@gmail.com>
2026-04-25 21:20:45 +0200

    make jk luasnip local jump instead of <Tab>

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,8 +81,6 @@ return {
                             else
                                 cmp.select_next_item()
                             end
-                        elseif luasnip.locally_jumpable(1) then
-                            luasnip.jump(1)
                         else
                             fallback()
                         end
@@ -90,12 +88,17 @@ return {
                     ["<S-Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
                             cmp.select_prev_item()
-                        elseif luasnip.locally_jumpable(-1) then
-                            luasnip.jump(-1)
                         else
                             fallback()
                         end
                     end, { "i", "s" }),
+                    ["jk"] = cmp.mapping(function (fallback)
+                        if luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" })
                 }),
             })
 
