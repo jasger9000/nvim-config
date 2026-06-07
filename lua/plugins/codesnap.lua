cd9fff674be602d2bfafb6186411edf64400fbe3
J. Gerhards <g1.jasger@gmail.com>
2026-06-07 17:15:15 +0200

    use opts instead of config in plugin setup when possible

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -5,16 +5,14 @@ return {
 	init = function()
 		vim.keymap.set('v', '<leader>sc', ':Silicon<CR>');
 	end,
-	config = function()
-		require("nvim-silicon").setup({
-			font = "Fira Code Nerd Font Mono=34;",
-			theme = "Monokai Extended",
-			background = "#45424b",
-			window_title = function()
-				return vim.fn.fnamemodify(
-					vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
-				)
-			end
-		})
-	end
+    opts = {
+        font = "Fira Code Nerd Font Mono=34;",
+        theme = "Monokai Extended",
+        background = "#45424b",
+        window_title = function()
+            return vim.fn.fnamemodify(
+                vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t"
+            )
+        end
+    },
 }
